f798849ed08f51b313b6b4e808022a0096640ece
larimar <larimarfondeur@gmail.com>
2022-09-07 11:21:30 -0400

    solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+puts "Destroying previous seeds..."
+
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -16,6 +22,12 @@ puts "Creating freebies..."
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+
+Freebie.create(item_name: "Water Bottle", value: 10, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+Freebie.create(item_name: "Hat", value: 12, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+Freebie.create(item_name: "Jumper", value: 3, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+Freebie.create(item_name: "Sweater", value: 40, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+Freebie.create(item_name: "Beanie", value: 8, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+Freebie.create(item_name: "Mousepad", value: 9, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
 
 puts "Seeding done!"
